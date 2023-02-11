9cc10da0b84f43b3123828095583f48751f0be95
mymachine707 <mymaxs441@gmail.com>
2023-02-11 20:51:45 +0500

    updated

---
+++ SQL/exampleDB1.sql
@@ -0,0 +1,10 @@
+create table exampleDB1 (
+    price numeric(6,2),
+    zip_code varchar(10),
+    comments text,
+    quantity int,
+    book_title varchar(50),
+    gender char(1),
+    meeting_day timestamp,
+    is_married boolean
+);
