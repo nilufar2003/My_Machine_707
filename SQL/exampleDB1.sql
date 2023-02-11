289cb50668f9b9cfa590bd64979eb3770bab946f
mymachine707 <mymaxs441@gmail.com>
2023-02-11 21:16:31 +0500

    updated

--- SQL/exampleDB1.sql
+++ SQL/exampleDB1.sql
@@ -8,3 +8,15 @@ create table exampleDB1 (
     meeting_day timestamp,
     is_married boolean
 );
+-- Topshiriqlar:
+
+-- 6ta ustundan tashkil topgan jadval yarating. Iloji boricha har xil turdagi ustunlardan tashkil topgan bo'lsin:
+-- VARCHAR
+-- TEXT
+-- INT
+-- TIMESTAMP
+-- BOOLEAN
+-- NUMERIC
+-- Bu jadvalga kamida 10ta ma'lumot kirgizing
+-- 5tasini Columnlar (ustunlar) ro'yxatini ko'rsatib.
+-- 5tasini esa columnlar ro'yxatini ko'rsatmasdan. Yani hamma columnlar uchun.
